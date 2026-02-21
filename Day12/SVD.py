4263beaca7ca97b7cb991fa6c707ad717f2a974c
prem-ramamoorthy <pkrprem2005@gmail.com>
2026-02-21 21:27:55 +0530

    .

--- Day12/SVD.py
+++ Day12/SVD.py
@@ -9,9 +9,9 @@ X = np.array([
 
 U, S, Vt = np.linalg.svd(X, full_matrices=False)
 
-print("U shape:", U.shape)     # (n_samples, r)
-print("S shape:", S.shape)     # (r,)
-print("Vt shape:", Vt.shape)   # (r, n_features)
+print("U shape:", U.shape)     # (n_samples, r) used for analysis of directions of samples
+print("S shape:", S.shape)     # (r,) used for analysis of importance of each direction
+print("Vt shape:", Vt.shape)   # (r, n_features) used for analysis of directions of features
 
 print("U:\n", U)
 print("S:\n", S)
